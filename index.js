2f19b274fe42e787f29f7d6f0ecb33e6a934c040
Erickkirui <erickkirui653@gmail.com>
2023-03-26 00:22:09 +0300

    Complete assignmet

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+// add new elements
+
+    let newHeader = document.querySelector('body')
+    newHeader.innerHTML  = "<h1>victory</h1>"
+    
+    
+    newHeader = document.querySelector('h1')
+    newHeader.id = "victory"
+    newHeader.innerHTML = "<p>Erick is the champion<p>"
